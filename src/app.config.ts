715906fa4432ff308da856ab5a7539a6ae7ba3a9
jon-stults <jon.stults@kalderos.com>
2021-03-10 00:42:46 -0600

    Update app.config.ts

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,5 @@
 let config;
 !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? config = 'http://localhost:8000' :
-    config = '';
+    config = 'https://pick-my-flick-api.herokuapp.com';
 
 export const apiConfig = config;
